35b8b3b79e053542a30912f15980da4153eb65d8
Imane Ouada <86524680+imane0x@users.noreply.github.com>
2024-09-13 01:10:38 +0100

    Update and rename PPO.py to train_ppo.py

--- PPO/train_ppo.py
+++ PPO/train_ppo.py
@@ -2,11 +2,11 @@ import argparse
 from stable_baselines3 import PPO
 import gym
 from custom_callback import CustomCallback
-from N_queens_env import NqueensEnv
-def train_ppo(board_size,env_name, total_timesteps, learning_rate, use_custom_callback):
+from N_queens_env import NQueensEnv
+def train_ppo(board_size, total_timesteps, learning_rate, use_custom_callback):
     # Create the environment
     #env = gym.make(env_name)
-    env = NqueensEnv(n = board_size)
+    env = NQueensEnv(n = board_size)
     
     # Initialize the PPO model
     model = PPO('MlpPolicy', env, learning_rate=learning_rate, verbose=1)
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     parser.add_argument("--board_size", type=int, required=True, help="Size of the board")
     parser.add_argument("--timesteps", type=int, default=10000, help="Total timesteps for training")
     parser.add_argument("--lr", type=float, default=0.0003, help="Learning rate")
-    parser.add_argument("--use_callback", action='store_true', help="Use custom callback for storing trajectories")
+    parser.add_argument("--use_callback", default = False, help="Use custom callback for storing trajectories")
 
     args = parser.parse_args()
 
-    train_ppo(args.env, args.timesteps, args.lr, args.use_callback)
+    train_ppo(args.board_size, args.timesteps, args.lr, args.use_callback)
